1032210fe7ff93b735322302a03d98016367278d
Biswajit sikdar <biswajitsikdar51@gmail.com>
2022-08-26 18:05:29 +0600

    Link Toggle issue solve

--- js/custom.js
+++ js/custom.js
@@ -7,3 +7,15 @@ toggle.onclick = function () {
   sidebar.classList.toggle("active");
   middle_none.classList.toggle("vanice");
 };
+
+// =====================sideBarLink===============
+$(document).ready(function () {
+  $("#toggle").click(function () {
+    $(".h_link").fadeToggle("slow");
+  });
+});
+$(document).ready(function () {
+  $("#toggle").click(function () {
+    $(".show_arrow").fadeToggle("slow");
+  });
+});
