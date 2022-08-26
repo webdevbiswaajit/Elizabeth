624a65fd9f62946d9de6f712c331c6324cb016bd
Biswajit sikdar <biswajitsikdar51@gmail.com>
2022-08-26 17:37:37 +0600

    SideBar issue solve

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,9 @@
 const toggle = document.getElementById("toggle");
 const sidebar = document.getElementById("dashboard_left");
+const middle_none = document.getElementById("middle_none");
 
 toggle.onclick = function () {
   toggle.classList.toggle("active");
   sidebar.classList.toggle("active");
+  middle_none.classList.toggle("vanice");
 };
