a6b5d8ba990c26ef9a8b02bb48fba291e2f5ccae
Biswajit sikdar <biswajitsikdar51@gmail.com>
2022-08-27 20:58:02 +0600

    Header Footer Message Box design

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 const toggle = document.getElementById("toggle");
 const sidebar = document.getElementById("dashboard_left");
 const middle_none = document.getElementById("middle_none");
+const msg_search = document.getElementById("msg_search");
 
 toggle.onclick = function () {
   toggle.classList.toggle("active");
@@ -19,3 +20,6 @@ $(document).ready(function () {
     $(".show_arrow").fadeToggle("slow");
   });
 });
+// =====================searchBox===========
+
+// =================ReportBox=====================
